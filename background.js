eef42b7bd45ff627c15a80995dc91a3dfc1c6f24
236618 Palewicz Bartosz <236618@edu.p.lodz.pl>
2021-11-27 17:53:34 +0100

    adding service worker

---
+++ background.js
@@ -0,0 +1,29 @@
+console.log('service worker is working');
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (!!tab.url)
+    if (
+      changeInfo.status === "complete" &&
+      tab.url.includes("w3schools.com/")
+    ) {
+      chrome.scripting
+        .insertCSS({
+          target: { tabId: tabId },
+          files: ["./css/dark.css"],
+        })
+        .then(() => {
+          console.log("css inserted");
+        })
+        .catch((err) => console.error("err: " + err));
+
+      chrome.scripting
+        .executeScript({
+          target: { tabId: tabId },
+          files: ["./js/content-script.js"],
+        })
+        .then(() => {
+          console.log("script executed");
+        })
+        .catch((err) => console.error("err: " + err));
+    }
+});
